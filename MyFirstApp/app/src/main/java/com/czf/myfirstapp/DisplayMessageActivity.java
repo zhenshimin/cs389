ce40c77493e0cf676b8f7b9631e3699a273e7ad6
yuanwj40730 <yuanwj40730@hundsun.com>
2022-10-03 14:30:09 +0800

    first app

---
+++ MyFirstApp/app/src/main/java/com/czf/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,21 @@
+package com.czf.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
